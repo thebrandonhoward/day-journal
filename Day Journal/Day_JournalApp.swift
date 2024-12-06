50de07eeb3ecded843a7d462f20e4e1b3abd09ed
thebrandonhoward <thebrandonhoward@outlook.com>
2024-12-06 14:10:53 -0600

    Initial Commit

---
+++ Day Journal/Day_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_JournalApp.swift
+//  Day Journal
+//
+//  Created by Brandon Howard on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day JournalTests/Day_JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Day_JournalTests.swift
+//  Day JournalTests
+//
+//  Created by Brandon Howard on 12/6/24.
+//
+
+import Testing
+@testable import Day_Journal
+
+struct Day_JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
